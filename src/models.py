27c4368e742102f3e53567409d7a34db0e664ed7
ff.mendieta <82422221+INS4NELY@users.noreply.github.com>
2025-05-19 15:50:22 +0000

    Finish Star Wars model

--- src/models.py
+++ src/models.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    last_name: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
@@ -17,3 +20,60 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    item_id: Mapped[int] = mapped_column(nullable=False)
+    item_type: Mapped[str] = mapped_column(String(60), nullable=False)
+    user = relationship('User', backref='favorites')
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "item_id": self.item_id,
+            "item_type": self.item_type,
+        }
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    birth_year: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(30), unique=False, nullable=False)
+    gender: Mapped[str] = mapped_column(String(30), unique=False, nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    height: Mapped[str] = mapped_column(String(30), unique=False, nullable=False)
+    mass: Mapped[str] = mapped_column(String(30), unique=False, nullable=False)
+    name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
+    skin_color: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+
+
+class Planet(db.Model):
+    __tablename__ = 'Planets'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    climate: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    diameter: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    gravity: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    name: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    orbital_period: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    population: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    rotation_period: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    surface_water: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    terrain: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicles'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cargo_capacity: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    consumables: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    cost_in_credits: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    crew: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    length: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    manufacturer: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    max_atmosphering_speed: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    model: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    name: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    passengers: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    vehicle_class: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
